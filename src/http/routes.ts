633adf94c360ea9cf30bb9b22dc6f1f4d8aca90f
Marco Sousa <marcotgousa@gmail.com>
2025-04-02 14:39:40 -0300

    Feat: Implement a user register route, register controller and password hash

---
+++ src/http/routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import { register } from "./controlers/register";
+
+export async function appRoutes(app:FastifyInstance) {
+
+  app.post('/users', register)
+  
+}
